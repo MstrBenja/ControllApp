9a8f8392ce20adcb10d568241e44c601c78abcdd
MstrBenja <sanhuez1240@gmail.com>
2023-10-25 21:16:09 -0300

    Change in information and adding a new Activity for the information for events

---
+++ app/src/main/java/com/example/controllapp/menu/eventos/infoEventos.java
@@ -0,0 +1,16 @@
+package com.example.controllapp.menu.eventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.controllapp.R;
+
+public class infoEventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_eventos);
+    }
+}
--- app/src/main/java/com/example/controllapp/menu/info/Information.java
+++ app/src/main/java/com/example/controllapp/menu/info/Information.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import com.example.controllapp.R;
 import com.example.controllapp.menu.Menu;
+import com.example.controllapp.menu.eventos.infoEventos;
 
 public class Information extends AppCompatActivity {
 
@@ -21,4 +22,9 @@ public class Information extends AppCompatActivity {
         Intent menu = new Intent(this, Menu.class);
         startActivity(menu);
     }// method
+
+    public void infoEvents(){
+        Intent info = new Intent(this, infoEventos.class);
+        startActivity(info);
+    }
 }// class
